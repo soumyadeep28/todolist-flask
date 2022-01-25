5d9d7e4a8026d8784a4067b27ee2b2262f98826f
ssengup <ssengup@gmail>
2022-01-26 01:24:16 +0530

    updated the html page and commited

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from http.client import HTTPResponse
+
+from flask import Flask ,render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def index(request):
-    return 
-
+def index():
+    return render_template("Base.html" )
 
 #-----------------run-------------
-app.run(Debug =True)
+if __name__ == "__main__":
+    app.run(debug =True)
+
+'''
+You can also set params
+FLASK_ENV=development
+FLASK_APP=main.py
+and do flask run commanmd to run the flask app
+'''
