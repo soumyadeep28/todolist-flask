d78df6591447f6da39f4f8e4fed348307469cb2a
ssengup <ssengup@gmail>
2022-01-26 02:13:21 +0530

    updated all the params and update and delte button are now working

--- main.py
+++ main.py
@@ -1,15 +1,80 @@
 from http.client import HTTPResponse
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask ,render_template, request ,redirect , url_for
 
-from flask import Flask ,render_template
+#------------------Define app---------------------
 app = Flask(__name__)
 
+#------set environment variable------------
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False 
+
+
+#------------------ Creating DB -------------------
+db =SQLAlchemy(app)
+
+
+#------------------Creating the Table for that opertaion --------------
+class Todo(db.Model):
+    id =db.Column(db.Integer , primary_key = True)
+    title = db.Column(db.String(100) )
+    complete = db.Column(db.Boolean)
+
+
+#------------------Url rendering and function and class calling -----------------
 @app.route('/')
 def index():
-    return render_template("Base.html" )
+    todolist = Todo.query.all()
+    print(todolist)
+    return render_template("Base.html" , params= todolist )
+
+
+@app.route('/add' , methods =['POST' , 'GET'])
+def add():
+    #add new item
+    title = request.form.get('mytext')
+    new_todo = Todo(title = title , complete = False )
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:todoid>' , methods= ['POST' , 'GET'])
+def complete(todoid):
+    todo = Todo.query.filter_by(id = todoid).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>' , methods= ['POST' , 'GET'])
+def delete(id):
+    todo = Todo.query.filter_by(id = id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
 
 #-----------------run-------------
 if __name__ == "__main__":
+    db.create_all()
+    '''
+    new_todo = Todo(title = 'todo1' ,complete = False)
+    db.session.add(new_todo)
+    db.session.commit() 
+    
+    '''
+
+
     app.run(debug =True)
 
 '''
